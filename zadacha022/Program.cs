775bac0da3639b16e8ace1d5d6d9f8cb965d0f82
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-13 19:45:40 +0400

    Zadacha 022

---
+++ zadacha022/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    double result = Math.Pow(i, 2);
+    Console.WriteLine($"Квадрат {i} = {result}");
+}
