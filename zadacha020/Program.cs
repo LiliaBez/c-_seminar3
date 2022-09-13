1a8ea960df38525aed93bba9ec9e53377434ff07
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-13 14:20:29 +0400

    Zadacha 020

---
+++ zadacha020/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход 
+//координаты двух точек и находит расстояние между 
+//ними в 2D пространстве.
+
+Console.WriteLine("Введите координаты первой точки X Y:");
+string firstXY = Console.ReadLine ();
+string[] parts1 = firstXY.Split (' ');
+int firstX = int.Parse (parts1[0]);
+int firstY = int.Parse (parts1[1]);
+Console.WriteLine("Введите координаты второй точки X Y:");
+string secondXY = Console.ReadLine ();
+string[] parts2 = secondXY.Split (' ');
+int secondX = int.Parse (parts2[0]);
+int secondY = int.Parse (parts2[1]);
+
+double dist = Math.Sqrt (Math.Pow(secondX-firstX, 2) + Math.Pow(secondY-firstY, 2));
+Console.WriteLine($"Расстояние между точками равно {dist:F3}");
